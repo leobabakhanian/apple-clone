50ca5fba72712ee1dc0bf98dac1eda5215fcbdc5
Leo Babakhanian <babakhanianleo@gmail.com>
2021-10-16 14:15:50 -0700

    deployed

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,9 @@
 import React from 'react';
 import './Header.css';
 import { Link } from "react-router-dom";
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import SearchIcon from '@mui/icons-material/Search';
 import AppleIcon from '@mui/icons-material/Apple';
 import SearchLens from './images/icons/search_lens.png';
-import ShoppingBag from './images/icons/shopping_bag.png'
+import ShoppingBag from './images/icons/shopping_bag.png';
 
 function Header() {
     return (
@@ -19,8 +17,6 @@ function Header() {
             <Link to ="/tv">TV</Link>
             <Link to ="/music">Music</Link>
             <Link to ="/support">Support</Link>
-            {/*<SearchIcon className="header__logo" />
-            <ShoppingBagIcon className="header__logo" /> */}
             <img className="header__search" src={SearchLens} alt="" />
             <img className="header__bag" src={ShoppingBag} alt="" />
         </div>
@@ -28,4 +24,3 @@ function Header() {
 }
 
 export default Header;
-
--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import Header from './Header';
 import './Home.css';
 import FirstBox from './FirstBox';
@@ -12,9 +12,13 @@ function Home() {
     return (
         <div className="home">
             <Header />
-            {/*min width should be 751 */}
             <div className="home__message">
-                <p>We look forward to welcoming you to our stores. Whether you <Link to ="/shop">shop in a store</Link> or <Link to ="/shop">shop online</Link>, our Specialists can help you buy the products you love.</p>
+                <p>
+                    We look forward to welcoming you to our stores. Whether 
+                    you <Link to ="/shop">shop in a store</Link> or <Link to ="/shop">
+                    shop online</Link>, our Specialists can help you buy the products 
+                    you love.
+                </p>
             </div>
             
             <ThirdBox />
@@ -23,7 +27,7 @@ function Home() {
             <FourthBox />
             <Footer />
         </div>
-    )
+    );
 }
 
-export default Home
+export default Home;
